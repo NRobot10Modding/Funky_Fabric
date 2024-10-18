d699bd706eea9d13589d6112f7d5feef1246d1be
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-10-18 16:43:42 +0100

    Initial commit

---
+++ src/main/java/net/nrobot10/funkyfabric/FunkyFabric.java
@@ -0,0 +1,16 @@
+package net.nrobot10.funkyfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FunkyFabric implements ModInitializer {
+	public static final String MOD_ID = "funky_fabric";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/nrobot10/funkyfabric/FunkyFabricClient.java
@@ -0,0 +1,10 @@
+package net.nrobot10.funkyfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunkyFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
